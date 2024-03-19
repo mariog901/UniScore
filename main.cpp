ab75ab1035b35eedb89bab266d668ea0ac6b9f32
Grigore Andrei-Mario <gmario9911@gmail.com>
2024-03-19 08:26:39 +0200

    Adaugare fisiere noi

--- main.cpp
+++ main.cpp
@@ -1,57 +1,181 @@
 #include <iostream>
-#include <array>
+#include <string>
+#include <vector>
+#include<fstream>
 
-#include <Helper.h>
+
+
+class Jucator {
+private:
+    std::string nume;
+    unsigned int varsta;
+    std::string pozitie;
+public:
+    Jucator(){};
+    Jucator(const std::string &nume,unsigned int varsta, std::string pozitie) : nume(nume) , varsta(varsta), pozitie(pozitie) {
+        this->nume = nume;
+    }
+
+    Jucator(const Jucator &jucator){
+        this->nume=jucator.nume;
+        this->varsta=jucator.varsta;
+        this->pozitie=jucator.pozitie;
+    }
+    Jucator &operator=(const Jucator &jucator) {
+        this->varsta = jucator.varsta;
+        this->nume = jucator.nume;
+        this->pozitie=jucator.pozitie;
+        return *this;
+    }
+
+    ~Jucator()=default;
+
+    const std::string &getNume() const{
+        return nume;
+    }
+    unsigned int getVarsta() const {
+        return varsta;
+    }
+    const std::string getPozitie() const {
+        return pozitie;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Jucator &jucator) {
+        out << "nume: " << jucator.nume << ", varsta: " << jucator.varsta<< ", Pozitia: "<<jucator.pozitie<<'\n';
+        return out;
+    }
+
+    friend std::istream &operator>>(std::istream &in,  Jucator &jucator) {
+        in >> jucator.nume >> jucator.varsta>>jucator.pozitie;
+        return in;
+    }
+
+
+
+
+};
+
+class Echipa {
+private:
+    std::string nume;
+    std::vector<Jucator> jucatori;
+
+public:
+    explicit Echipa(const std::string &numeEchipa, const std::vector<Jucator> &fotbalisti) : nume(numeEchipa),jucatori(fotbalisti) {}
+    Echipa(const std::string &nume):nume(nume){}
+    Echipa() {};
+
+    ~Echipa()=default;
+
+    const std::string getNume() const {
+        return nume;
+    }
+
+    const std::vector<Jucator> getPlayers() const {
+        return jucatori;
+    };
+
+
+    void addPlayer(const Jucator&jucator){
+        jucatori.push_back(jucator);
+    }
+    void afisare() const{
+        int i=1;
+        std::cout<<"Echipa " << nume << " are jucatorii: "<<std::endl;
+        for(const auto &jucator:jucatori){
+            std::cout<<i<<". "<<jucator.getNume()<< " in varsta de "<<jucator.getVarsta()<<" ani "<< "si joaca pe pozitia de "<<jucator.getPozitie()<<std::endl;
+            i++;
+        }
+    }
+    friend std::ostream &operator<<(std::ostream &out, const Echipa &echipa) {
+        out << echipa.nume << ""<<std::endl;
+        for (const Jucator &jucator :echipa.jucatori){
+            out<<jucator<<"";
+        }
+        return out;
+    }
+
+
+};
+
+
+
+class Campionat {
+private:
+    std::string nume;
+    std::vector<Echipa> echipe;
+
+public:
+    explicit Campionat(const std::string &numeCampionat,const std::vector<Echipa> &echipe): nume(numeCampionat),echipe(echipe){}
+
+    Campionat() {};
+    ~Campionat()=default;
+
+    const std::string &getNume() const{
+        return nume;
+    }
+
+    const std::vector<Echipa> getEchipe() const {
+        return echipe;
+    }
+
+
+    void addTeam(const Echipa &echipa){
+        echipe.push_back(echipa);
+    }
+    friend std::ostream &operator<<(std::ostream &out, const Campionat &campionat) {
+        out << campionat.nume ;
+        for (const Echipa &echipa:campionat.echipe){
+            out<<echipa<<"";
+        }
+        return out;
+    }
+//    bool checkChampionship(const Echipa &echipa) const {
+//        for (const auto& echipa : echipe) {
+//            if (Echipa.getNume() == echipa.getNume()) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
+};
+
+
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+//        Jucator Olaru("Darius Olaru",25, "CM");
+//        Jucator Coman( "Florinel Coman",25, "LW");
+//        std::cout << Olaru.getNume() << " " << Olaru.getVarsta() << " "  << Olaru.getPozitie() << "\n";
+//        std::cout << Coman.getNume() << " " << Coman.getVarsta() << " "  << Coman.getPozitie() << "\n";
+    Echipa FCSB("FCSB");
+    Echipa CFR("CFR");
+    Echipa Craiova("Craiova");
+    Jucator Coman("Florinel Coman", 25, "LW");
+    Jucator Olaru("Darius Olaru",25, "CM");
+    Jucator Muhar("Karlo Muhar" , 30 , "CDM");
+    Jucator Mitrita("Alexandru Mitrita",29,"LW");
+    Jucator Ivan ("Andrei Ivan" ,26, "ST");
+    Jucator Markovic ("Jovan Markovic" ,22, "ST");
+    Jucator Baiaram("Stefan Baiaram" , 20, "CAM");
+    FCSB.addPlayer(Coman);
+    FCSB.addPlayer(Olaru);
+    CFR.addPlayer(Muhar);
+    Craiova.addPlayer(Mitrita);
+    Craiova.addPlayer(Ivan);
+    Craiova.addPlayer(Markovic);
+    Craiova.addPlayer(Mitrita);
+    Campionat Superliga;
+    Superliga.addTeam(FCSB.getNume());
+    Superliga.addTeam(CFR.getNume());
+    Superliga.addTeam(Craiova.getNume());
+    std::cout<<FCSB<<"\n";
+    std::cout<<CFR<<"\n";
+    std::cout<<Craiova<<"\n";
+
+    std::cout<<"Echipele din liga 1 sunt:"<<"\n"<<Superliga<<"\n";
+    Craiova.afisare();
     return 0;
 }
+
+
